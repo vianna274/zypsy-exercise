b9dd432cc6d454c926e4c46c4b52c3cd63ecb0fb
Leonardo Feiteira <leonardo.vianna.f@gmail.com>
2024-11-14 13:25:16 -0300

    ✨ Add RadioButton

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from 'styled-components';
 import theme from './theme';
 import Button from './components/Button';
+import RadioButton from './components/RadioButton';
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <>
         <Button>Click me</Button>
         <Button variant="secondary">Click me</Button>
+        <RadioButton label="Radio button" value="radio" checked={false} onChange={() => {}} />
+        <RadioButton label="Radio button" value="radio" checked={true} onChange={() => {}} />
       </>
     </ThemeProvider>
   );
---
+++ frontend/src/components/RadioButton.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+type Props = {
+  label: string;
+  value: string;
+  checked: boolean;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const RadioContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const HiddenRadio = styled.input.attrs({ type: 'radio' })`
+  display: none;
+`;
+
+const StyledRadio = styled.span<{ checked: boolean }>`
+  width: 20px;
+  height: 20px;
+  border: 1px solid ${props => props.theme.colors.accent};
+  border-radius: 26px;
+  display: inline-block;
+  position: relative;
+  margin-right: ${props => props.theme.spacing(1)};
+
+  ${props =>
+    props.checked &&
+    css`
+      background-color: white;
+
+      &::after {
+        content: '';
+        width: 12px;
+        height: 12px;
+        border-radius: 16px;
+        background: ${props => props.theme.colors.primary};
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+      }
+    `}
+`;
+
+const Label = styled.span`
+  font-size: 12px;
+  line-height: 20px;
+  font-weight: 400;
+  color: ${props => props.theme.colors.foreground};
+`;
+
+const RadioButton: React.FC<Props> = ({ label, value, checked, onChange }) => {
+  return (
+    <RadioContainer>
+      <HiddenRadio
+        value={value}
+        checked={checked}
+        onChange={onChange}
+      />
+      <StyledRadio checked={checked} />
+      <Label>{label}</Label>
+    </RadioContainer>
+  );
+};
+
+export default RadioButton; 
