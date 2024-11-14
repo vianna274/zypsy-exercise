8317f997a99f984959957b6a77e7e9a0081ef212
Leonardo Feiteira <leonardo.vianna.f@gmail.com>
2024-11-14 15:11:52 -0300

    ✨ Add StarIcon Component

---
+++ frontend/src/components/StarIcon.tsx
@@ -0,0 +1,24 @@
+const StarIcon = () => {
+  return (
+    <svg
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M9 1.41333L11.4458 6.4075L16.9167 7.21333L12.9583 11.0983L13.8925 16.5867L9 13.9942L4.1075 16.5867L5.04167 11.0983L1.08333 7.21333L6.55333 6.4075L9 1.41333Z"
+        fill="#E3E3E3"
+        stroke="#E3E3E3"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default StarIcon;
